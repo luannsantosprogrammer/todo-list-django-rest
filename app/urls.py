62f6977f34ea93a922810c654434b1db0cfcf75b
luannsantosprogrammer <luannrodrigodesouzasantos@gmail.com>
2025-09-18 11:15:35 -0300

    first commit

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import TaskDetailView,TaskListCreateView
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("tasks/", TaskListCreateView.as_view(), name='task-list'),
-    path("tasks/<int:pk>/", TaskDetailView.as_view(), name='task-detail')
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name='task-detail'),
+    path('tasks/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('tasks/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
